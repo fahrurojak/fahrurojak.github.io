f00215e437f72ac99bcad769d543878d5a87ff5d
Fahru Rojak <65585044+fahrurojak@users.noreply.github.com>
2023-05-15 17:20:44 +0700

    Update tictac.js

--- js/tictac.js
+++ js/tictac.js
@@ -1,240 +1,110 @@
-'use strict'
+"use strict";
 
-const statusDisplay = document.getElementById('status')
-const countField = document.getElementById('numberTurns')
-const startBox = document.getElementById('startBox')
-const playField = document.getElementById('field')
-const player1_name = document.getElementById('player1_name')
-const player2_name = document.getElementById('player2_name')
-const player1 = document.getElementById('player1')
-const player2 = document.getElementById('player2')
+const statusDisplay = document.querySelector("#status");
+const count = document.querySelector("#numberTurns");
 
-let gameActive = true
-let currentPlayer = 'X'
-let gameState = []
-let cols, rows, steps, counter = 0
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
 
-const winnMessage = () => `${currentPlayer} has won!`
-const nobodyWinsMessage = () => `it's a draw!`
+const winnMessage = () => `${currentPlayer} has won!`;
+const drawMessage = () => `it's a draw!`;
 
-// ----------------------------------  START GAME
-let checkInput = (input) => {
-    input = +input
-    input = (input < 3)
-        ? 3
-        : (input > 10)
-            ? 10
-            : input
-    return input
-}
-let createMatrix = () => {
-    let arr
-    for (let i = 0; i < rows; i++) {
-        arr = []
-        for (let j = 0; j < cols; j++) {
-            arr[j] = 0
-        }
-        gameState[i] = arr
-    }
-    console.log(gameState)
-}
-let drawField = () => {
-    let cellSize = window.innerHeight * 0.5 / cols
-    let box = document.createElement('div')
-    box.setAttribute('id', 'container')
+const winnLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-    let cell, row
-    for (let i = 0; i < rows; i++) {
-        row = document.createElement('div')
-        row.className = 'row'
-        for (let j = 0; j < cols; j++) {
-            cell = document.createElement('div')
-            cell.setAttribute('id', `${i}_${j}`)
-            cell.className = 'cell'
-            cell.style.width =
-                cell.style.height =
-                    cell.style.lineHeight = `${cellSize}px`
-            cell.style.fontSize = `${cellSize / 16}em`
-            row.appendChild(cell)
-        }
-        box.appendChild(row)
-    }
-    playField.appendChild(box)
-}
+function handlePlayerTurn() {
+    const p1 = document.querySelector("#player1"),
+        p2 = document.querySelector("#player2");
 
-let handleStart = () => {
-    player1.innerHTML = player1_name.value === '' ? 'Player \'X\'' : player1_name.value
-    player2.innerHTML = player2_name.value === '' ? 'Player \'O\'' : player2_name.value
-    cols = checkInput(document.getElementById('columns').value)
-    rows = checkInput(document.getElementById('rows').value)
-    steps = checkInput(document.getElementById('steps').value)
-    createMatrix()
-    drawField()
-    startBox.className = 'hidden'
-    handlePlayerSwitch()
-    document.querySelectorAll('.cell')
-        .forEach(cell => cell.addEventListener('click', handleClick))
-}
-
-// ---------------------------------- WINNER ALGORITHM
-
-let isWinning = (y, x) => {
-    let winner = currentPlayer === 'X' ? 1 : 2,
-        length = steps * 2 - 1,
-        radius = steps - 1,
-        countWinnMoves, winnCoordinates
-
-    // horizontal
-    countWinnMoves = 0
-    winnCoordinates = []
-    for (let i = y, j = x - radius, k = 0; k < length; k++, j++) {
-        if (i >= 0 && i < rows && j >= 0 && j < cols &&
-            gameState[i][j] === winner && gameActive) {
-            winnCoordinates[countWinnMoves++] = [i, j]
-            if (countWinnMoves === steps) {
-                winnActions(winnCoordinates)
-                return
-            }
-        } else {
-            countWinnMoves = 0
-            winnCoordinates = []
-        }
-    }
-
-    // vertical
-    countWinnMoves = 0
-    winnCoordinates = []
-    for (let i = y - radius, j = x, k = 0; k < length; k++, i++) {
-        if (i >= 0 && i < rows && j >= 0 && j < cols &&
-            gameState[i][j] === winner && gameActive) {
-            winnCoordinates[countWinnMoves++] = [i, j]
-            if (countWinnMoves === steps) {
-                winnActions(winnCoordinates)
-                return
-            }
-        } else {
-            countWinnMoves = 0
-            winnCoordinates = []
-        }
-    }
-
-    // oblique to the right
-    countWinnMoves = 0
-    winnCoordinates = []
-    for (let i = y - radius, j = x - radius, k = 0; k < length; k++, i++, j++) {
-        if (i >= 0 && i < rows && j >= 0 && j < cols &&
-            gameState[i][j] === winner && gameActive) {
-            winnCoordinates[countWinnMoves++] = [i, j]
-            if (countWinnMoves === steps) {
-                winnActions(winnCoordinates)
-                return
-            }
-        } else {
-            countWinnMoves = 0
-            winnCoordinates = []
-        }
-    }
-
-    // oblique to the left
-    countWinnMoves = 0
-    winnCoordinates = []
-    for (let i = y - radius, j = x + radius, k = 0; k < length; k++, i++, j--) {
-        if (i >= 0 && i < rows && j >= 0 && j < cols &&
-            gameState[i][j] === winner && gameActive) {
-            winnCoordinates[countWinnMoves++] = [i, j]
-            if (countWinnMoves === steps) {
-                winnActions(winnCoordinates)
-                return
-            }
-        } else {
-            countWinnMoves = 0
-            winnCoordinates = []
-        }
-    }
-}
-
-// ----------------------------------  GAME ONGOING
-
-let handlePlayerSwitch = () => {
-    if (currentPlayer === 'X') {
-        player1.style.background = '#8458B3'
-        player2.style.background = '#d0bdf4'
+    if (currentPlayer === "X") {
+        p1.style.background = "#8458B3";
+        p2.style.background = "#d0bdf4";
     } else {
-        player1.style.background = '#d0bdf4'
-        player2.style.background = '#8458B3'
+        p1.style.background = "#d0bdf4";
+        p2.style.background = "#8458B3";
     }
 }
 
-let isMovesLeft = () => {
-    if (counter === cols * rows) {
-        statusDisplay.innerHTML = nobodyWinsMessage()
-        gameActive = false
+function handleClick(event) {
+    let clickedIndex = Number(event.target.getAttribute("id"));
+
+    if (gameState[clickedIndex] !== "" || !gameActive) return;
+    gameState[clickedIndex] = currentPlayer;
+    event.target.innerHTML = currentPlayer;
+    count.innerHTML = +count.innerHTML + 1;
+    handleResult();
+}
+
+function handleResult() {
+    let roundWon = false,
+        winLine,
+        a,
+        b,
+        c,
+        i;
+
+    for (i = 0; i < 8; ++i) {
+        winLine = winnLines[i];
+        a = gameState[winLine[0]];
+        b = gameState[winLine[1]];
+        c = gameState[winLine[2]];
+        if (a === b && b === c && c !== "") {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon || !gameState.includes("")) {
+        if (roundWon) {
+            statusDisplay.innerHTML = winnMessage();
+            statusDisplay.style.color = "#139de2";
+            winColors(winLine);
+        } else statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    handlePlayerTurn();
+}
+
+function winColors(line) {
+    console.log(`${line}`);
+    for (let i = 0; i < 3; ++i) {
+        let cell = document.getElementById(`${line[i]}`);
+        cell.style.color = "#139de2";
+        cell.style.fontSize = "80px";
     }
 }
 
-let handleClick = (event) => {
-    let clickedIndex = event.target.getAttribute('id').split('_');
-    let i = +clickedIndex[0]
-    let j = +clickedIndex[1]
+function handleRestart() {
+    gameActive = true;
+    currentPlayer = "X";
+    count.innerHTML = "0";
+    statusDisplay.innerHTML = "";
+    statusDisplay.style.color = "black";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    handlePlayerTurn();
 
-    if (gameState[i][j] !== 0 || !gameActive)
-        return
-
-    gameState[i][j] = (currentPlayer === 'X') ? 1 : 2
-    event.target.innerHTML = currentPlayer
-    countField.innerHTML = `${++counter}`
-
-    isWinning(i, j)
-    isMovesLeft()
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
-    handlePlayerSwitch()
-
-    // console.log(gameState)
+    document.querySelectorAll(".cell").forEach((cell) => {
+        cell.innerHTML = "";
+        cell.style.color = "#232d55";
+        cell.style.fontSize = "60px";
+    });
 }
 
-// ----------------------------------  SHOW WINNING RESULTS
+handlePlayerTurn();
 
-function winnActions(winner) {
-    console.log(winner)
-
-    gameActive = false
-    statusDisplay.innerHTML = winnMessage()
-    statusDisplay.style.color = '#139de2'
-
-    let cell
-    for (let i = 0; i < winner.length; i++) {
-        cell = document.getElementById(`${winner[i][0]}_${winner[i][1]}`)
-        cell.style.color = '#139de2'
-    }
-}
-
-// ----------------------------------  RESET GAME
-let handlePlayAgain = () => {
-    gameActive = true
-    currentPlayer = 'X'
-    counter = 0
-    countField.innerHTML = '0'
-    statusDisplay.innerHTML = ''
-    statusDisplay.style.color = 'black'
-    player1.style.background = player2.style.background = '#d0bdf4'
-    playField.removeChild(document.getElementById('container'))
-    handleStart()
-}
-
-let handleRestart = () => {
-    gameActive = true
-    currentPlayer = 'X'
-    counter = 0
-    countField.innerHTML = '0'
-    statusDisplay.innerHTML = ''
-    statusDisplay.style.color = 'black'
-    player1.style.background = player2.style.background = '#d0bdf4'
-    player1_name.value = player2_name.value = ''
-    player1.innerHTML = player2.innerHTML = '-'
-    startBox.className = 'sidebar'
-    playField.removeChild(document.getElementById('container'))
-}
-
-document.querySelector('#start').addEventListener('click', handleStart)
-document.querySelector('#playAgain').addEventListener('click', handlePlayAgain)
-document.querySelector('#restart').addEventListener('click', handleRestart)
+document
+    .querySelectorAll(".cell")
+    .forEach((cell) => cell.addEventListener("click", handleClick));
+document.querySelector("#restart").addEventListener("click", handleRestart);
